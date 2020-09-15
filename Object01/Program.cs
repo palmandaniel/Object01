0f2d0324af3b784e0e70f96a15cdf10261b480c7
palmandaniel <palman.daniel47@gmail.com>
2020-09-15 09:45:47 +0200

    Add project files.

---
+++ Object01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object01
+{
+    class Ember
+    {
+        public string nev;
+        public int eletkor;
+
+        public string Bemutatkozas()
+        {
+            return (nev + " vagyok " + eletkor + " eves ");
+        //    string valasz = $"{nev} vagyok {eletkor} éves";
+        //    return valasz;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember Pisti = new Ember();
+
+            Pisti.nev = "Nagy Pisti";
+            Pisti.eletkor = 20;
+
+            Pisti.eletkor++;
+            Ember Eva = new Ember();
+
+            Eva.nev = "Nagy Éva";
+            Eva.eletkor = 18;
+            Console.WriteLine(Pisti.Bemutatkozas());
+            Console.WriteLine(Eva.Bemutatkozas());
+
+            Console.ReadKey();
+        }
+    }
+}
