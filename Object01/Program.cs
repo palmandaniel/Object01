4ff1e1c72aa9d4eb499c168d035da48ad8e75163
palmandaniel <palman.daniel47@gmail.com>
2020-09-15 10:08:33 +0200

    Ember class + teglalap clas

--- Object01/Program.cs
+++ Object01/Program.cs
@@ -19,23 +19,47 @@ namespace Object01
         }
     }
 
+    class Teglalap
+    {
+        public int a;
+        public int b;
+
+        public int Kerulet()
+        {
+            int kerulet = 2 * a + 2 * b;
+            return kerulet;
+        }
+
+        public int Terulet()
+        {
+            int terulet = a * b;
+            return terulet;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Ember Pisti = new Ember();
+            //Ember Pisti = new Ember();
 
-            Pisti.nev = "Nagy Pisti";
-            Pisti.eletkor = 20;
+            //Pisti.nev = "Nagy Pisti";
+            //Pisti.eletkor = 20;
 
-            Pisti.eletkor++;
-            Ember Eva = new Ember();
+            //Pisti.eletkor++;
+            //Ember Eva = new Ember();
 
-            Eva.nev = "Nagy Éva";
-            Eva.eletkor = 18;
-            Console.WriteLine(Pisti.Bemutatkozas());
-            Console.WriteLine(Eva.Bemutatkozas());
+            //Eva.nev = "Nagy Éva";
+            //Eva.eletkor = 18;
+            //Console.WriteLine(Pisti.Bemutatkozas());
+            //Console.WriteLine(Eva.Bemutatkozas());
+            
+            //Teglalap t = new Teglalap();
 
+            //t.a = 5;
+            //t.b = 10;
+            //Console.WriteLine("kerület ",t.Kerulet());
+            //Console.WriteLine("terület ",t.Terulet());
+            
             Console.ReadKey();
         }
     }
